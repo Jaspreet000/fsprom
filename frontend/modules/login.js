f7a14fe566b37df514ce582f73f5bb9f57c2d456
Jaspreet Singh <jaspreetsingh44555@gmail.com>
2023-09-25 22:57:07 +0530

    Update login.js

--- frontend/modules/login.js
+++ frontend/modules/login.js
@@ -23,7 +23,7 @@ async function userform(){
     };
 
     try {
-        const response = await fetch("http://localhost:8080/register",{
+        const response = await fetch("https://patchdquiz.onrender.com/register",{
             method:"POST",
             headers:{
                 "Content-Type":"application/json",
@@ -52,7 +52,7 @@ async function usersign(){
         password:document.getElementById("password1").value
     };
     try {
-        const response = await fetch("http://localhost:8080/login",{
+        const response = await fetch("https://patchdquiz.onrender.com/login",{
             method:"POST",
             headers:{
             "Content-Type":"application/json",
