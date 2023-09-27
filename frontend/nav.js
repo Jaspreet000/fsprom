406ba9b6978aa0dd386dd4a73288052dc872048a
Jaspreet Singh <jaspreetsingh44555@gmail.com>
2023-09-27 18:25:21 +0530

    Delete frontend directory

--- frontend/nav.js
+++
@@ -1,7 +0,0 @@
-const btnnn = document.getElementById('btnham');
-
-function classAdd(){
- const nav = document.getElementById('nav');
-  nav.classList.toggle('active');
-}
- btnnn.addEventListener('click', classAdd);
